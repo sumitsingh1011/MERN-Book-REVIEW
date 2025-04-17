f3343b16c32fb235721f3da4a4d1c24712d035ff
sumitsingh1101 <sumitsingh17478@gmail.com>
2025-04-18 00:47:56 +0530

    Remove all files from index to re-track with updated .gitignore

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="bg-light text-center py-4 mt-4">
-        <p>&copy; 2023 Book Review Platform</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
